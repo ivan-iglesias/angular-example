299cf2ee0502a9c1b8fb1ce004d014b828249646
ivan-iglesias <iiglesiasbilbao@gmail.com>
2022-03-21 21:05:39 +0100

    form model

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 
 @Component({
@@ -10,8 +12,19 @@ export class CartComponent {
 
   items = this.cartService.getItems();
 
-  constructor(
-    private cartService: CartService
-  ) { }
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
 
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
+  ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
